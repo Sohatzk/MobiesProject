0997945d296a47d2a06a856fe71906f1cab12bda
Bohdan <bohdan.sokhatskyi@quest.com>
2023-02-01 00:20:27 +0200

    Add Angular basic skeleton

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -7,6 +7,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
+builder.Services.AddCors();
+
 builder.Services.AddDbContext<MovieContext>(options =>
 {
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
@@ -15,6 +17,8 @@ builder.Services.AddDbContext<MovieContext>(options =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseCors(builder
+    => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
 
 app.MapControllers();
 
